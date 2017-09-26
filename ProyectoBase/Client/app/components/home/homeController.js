85c092c8c2969949d7c73c5686f157483568f2b4
gchervet <gchervet@kennedy.edu.ar>
2017-09-26 17:10:51 -0300

    Menu del lado de angular (40%)

--- ProyectoBase/Client/app/components/home/homeController.js
+++ ProyectoBase/Client/app/components/home/homeController.js
@@ -2,16 +2,19 @@
   .controller('homeController', function ($scope, $rootScope, $location, $sessionStorage, utilityService, Auth) {
 
       var homeController = this;
+      homeController.menuGroupList = [];
 
       homeController.init = function () {
-          
+
           Auth.tokenCookieExists();
-          
+
           if ($sessionStorage.user == null) {
               $rootScope.logout();
           }
 
           homeController.userFullName = $sessionStorage.user.name;
+
+          homeController.getAllMenu();
 
           //if (Auth.userHasPermission(["administration"])) {
           //    var userName = Auth.currentUser().name;
@@ -42,6 +45,25 @@
           }
 
           utilityService.callHttp({ method: "GET", url: "/api/uniAlumno/GetByLegajo?legajo=12345", callbackSuccess: testCallback, callbackError: testErrorCallback });
+      };
+
+      homeController.getAllMenu = function () {
+          Auth.tokenCookieExists();
+
+
+          var getAllMenuCallback = function (response) {
+              debugger;
+              if (response) {
+                  homeController.menuGroupList = response.data;
+              }
+          };
+
+          var getAllMenuErrorCallback = function (response) {
+              debugger;
+
+          }
+
+          utilityService.callHttp({ method: "GET", url: "/api/menu/getall", callbackSuccess: getAllMenuCallback, callbackError: getAllMenuErrorCallback });
       };
 
   });
