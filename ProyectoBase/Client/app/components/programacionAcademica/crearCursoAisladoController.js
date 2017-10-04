bc8b1bc9a55728d481f79a6f38a19b9d240d60c3
gchervet <gchervet@kennedy.edu.ar>
2017-10-04 17:06:06 -0300

    Se generan  los primeros formularios de cursos con datos de json falsos.
    Se debe obtener la lista de carreras, y a partir de ahi obtener la lista de materias.

--- ProyectoBase/Client/app/components/programacionAcademica/crearCursoAisladoController.js
+++ ProyectoBase/Client/app/components/programacionAcademica/crearCursoAisladoController.js
@@ -6,6 +6,47 @@
       crearCursoAisladoController.init = function () {
 
           Auth.tokenCookieExists();
+          debugger;
+          crearCursoAisladoController.carreraList =
+              [
+                   {
+                       name: "CARRERA 1",
+                       code: "C1"
+                   },
+                   {
+                       name: "CARRERA 2",
+                       code: "C2"
+                   },
+                   {
+                       name: "CARRERA 3",
+                       code: "C3"
+                   },
+                   {
+                       name: "CARRERA 4",
+                       code: "C4"
+                   }
+              ];
+
+
+          crearCursoAisladoController.materiaList =
+              [
+                   {
+                       name: "MATERIA 1",
+                       code: "M1"
+                   },
+                   {
+                       name: "MATERIA 2",
+                       code: "M2"
+                   },
+                   {
+                       name: "MATERIA 3",
+                       code: "M3"
+                   },
+                   {
+                       name: "MATERIA 4",
+                       code: "M4"
+                   }
+              ];
 
           if ($sessionStorage.user == null) {
               $rootScope.logout();
