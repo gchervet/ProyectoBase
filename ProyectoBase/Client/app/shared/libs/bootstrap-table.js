5df2df620ae9704df59f241785cad3fb8c7753b6
gchervet <gchervet@kennedy.edu.ar>
2017-11-13 11:03:42 -0300

    Bootstrap table:
    Se modifican los label de inglés a español

--- ProyectoBase/Client/app/shared/libs/bootstrap-table.js
+++ ProyectoBase/Client/app/shared/libs/bootstrap-table.js
@@ -464,37 +464,37 @@
 
     BootstrapTable.LOCALES['en-US'] = BootstrapTable.LOCALES.en = {
         formatLoadingMessage: function () {
-            return 'Loading, please wait...';
+            return 'Cargando...';
         },
         formatRecordsPerPage: function (pageNumber) {
-            return sprintf('%s rows per page', pageNumber);
+            return sprintf('%s filas por página', pageNumber);
         },
         formatShowingRows: function (pageFrom, pageTo, totalRows) {
-            return sprintf('Showing %s to %s of %s rows', pageFrom, pageTo, totalRows);
+            return sprintf('Mostrando %s a %s de %s las filas', pageFrom, pageTo, totalRows);
         },
         formatDetailPagination: function (totalRows) {
-            return sprintf('Showing %s rows', totalRows);
+            return sprintf('Mostrando %s filas', totalRows);
         },
         formatSearch: function () {
-            return 'Search';
+            return 'Buscar';
         },
         formatNoMatches: function () {
-            return 'No matching records found';
+            return 'No se encontraron registros';
         },
         formatPaginationSwitch: function () {
-            return 'Hide/Show pagination';
+            return 'Mostrar/Ocultar paginación';
         },
         formatRefresh: function () {
-            return 'Refresh';
+            return 'Refrescar';
         },
         formatToggle: function () {
             return 'Toggle';
         },
         formatColumns: function () {
-            return 'Columns';
+            return 'Columnas';
         },
         formatAllRows: function () {
-            return 'All';
+            return 'Todo';
         }
     };
 
